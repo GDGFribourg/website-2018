30c5614448eeb1bad25efc9509864c01372ece9a
Jacques Supcik <jacques@supcik.net>
2018-04-09 15:54:28 +0200

    First steps for event list from meetup

--- static/js/gdg.js
+++ static/js/gdg.js
@@ -1,14 +1,28 @@
-(function($) {
-    "use strict";
+$('#collapseCoc').on('hidden.bs.collapse', function () {
+    'use strict';
+    $('#morebtn').html('more...');
+});
 
-    $('#collapseCoc').on('hidden.bs.collapse', function () {
-        $('#morebtn').html('more...');
-    });
+$('#collapseCoc').on('shown.bs.collapse', function () {
+    'use strict';
+    $('#morebtn').html('less...');
+});
 
-    $('#collapseCoc').on('shown.bs.collapse', function () {
-        $('#morebtn').html('less...');
-    });
+customElements.define('meetup-event', class extends HTMLElement {
+    constructor() {
+        super();
+        const title = this.getAttribute("title");
+        const date = this.getAttribute("date");
+        const url = this.getAttribute("url");
 
-})(jQuery);
+        this._div = document.createElement('div');
+        const s0 = this._div.appendChild(document.createElement('span'));
+        s0.innerText = title;
+    }
 
-  	
+    connectedCallback () {
+        this.appendChild(this._div);
+    }
+
+
+})
